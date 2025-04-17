a1bfebeb400f8c5be3bfe25f2a864aa886c67727
Pravin <pravin.padalkar@techturbine.com>
2025-04-17 17:35:05 +0530

    Implement basic card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+import React, { useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter, Image } from "@heroui/react";
+import img from "/profile.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+
 export default function ProductCard() {
+  const [isLiked, setIsLiked] = useState(false);
   return (
-    <Card className="max-w-[300px]">
-      <CardHeader>header</CardHeader>
+    <Card className="w-[300px]">
+      <CardHeader>
+        <Image src={img} alt="ProductImg" radius="lg" />
+        <FontAwesomeIcon
+          icon={isLiked ? solidHeart : regularHeart}
+          onClick={()=>setIsLiked(!isLiked)}
+          className="fa-xl absolute top-4 right-4 z-10 cursor-pointer"
+        />
+      </CardHeader>
       <CardBody>body</CardBody>
       <CardFooter>footer</CardFooter>
     </Card>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard'
+import React from "react";
+import ProductCard from "../components/ProductCard";
 
 export default function ProductList() {
-
   return (
-    <section >
-      <h1 className='text-xl font-bold'>Products List</h1>
-     <div className='grid grid-cols-4 gap-4'>
-     <ProductCard/>
-     
-     </div>
+    <section>
+      <h1 className="text-2xl font-bold">Products List</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <ProductCard />
+      </div>
     </section>
-  )
+  );
 }
