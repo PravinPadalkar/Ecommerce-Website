66bf097746aa6c235d972627aebbc5f338d48052
Pravin <pravin.padalkar@techturbine.com>
2025-04-18 17:40:05 +0530

    Implement CartList Page and Mytable utility function

--- src/pages/CartList.jsx
+++ src/pages/CartList.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import CartTable from "../components/CartTable";
 
 export default function CartList() {
+ 
   return (
-    <div>CartList</div>
-  )
+    <>
+      <p className="text-2xl font-bold  text-custom my-4 bg-custom-400">CartList</p>
+      <CartTable/>
+    </>
+  );
 }
