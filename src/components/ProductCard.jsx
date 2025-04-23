e2a62722009ff3ce508245873e9c0f4026882ecc
Pravin <pravin.padalkar@techturbine.com>
2025-04-23 12:40:20 +0530

    Fixed wishlist toaster logic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Card, CardHeader, CardBody, CardFooter, Image, Divider, Button, addToast } from "@heroui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Divider,
+  Button,
+  addToast,
+} from "@heroui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
@@ -27,21 +36,36 @@ export default function ProductCard({ id, imageUrl, title, category, price, rati
     addToast({
       title: "Added To Cart!!",
       description: `Product with ID : ${id} Added To Cart`,
-      color:'success'
+      color: "success",
     });
   };
   const handleAddToWish = (id, imageUrl, title, price) => {
     setWishList((prevState) => {
       const existingItem = prevState.find((item) => item.id == id);
       if (!existingItem) {
+        setTimeout(
+          () =>
+            addToast({
+              title: "Added To Wish!!",
+              description: `Product with ID : ${id} Added To WishList`,
+              color: "success",
+            }),
+          0
+        );
+        setIsLiked(true)
         return [...prevState, { id, imageUrl, title, price }];
+      } else {
+        setTimeout(
+          () =>
+            addToast({
+              title: "Item Already Present",
+              description: `Product with ID : ${id} Already Available To WishList`,
+              color: "danger",
+            }),
+          0
+        );
+        return prevState;
       }
-      return prevState
-    });
-    addToast({
-      title: "Added To Wish!!",
-      description: `Product with ID : ${id} Added To WishList`,
-      color:'success'
     });
   };
   return (
@@ -50,7 +74,7 @@ export default function ProductCard({ id, imageUrl, title, category, price, rati
         <Image className="h-[170px] object-fill" src={imageUrl} alt="ProductImg" radius="lg" />
         <FontAwesomeIcon
           icon={isLiked ? solidHeart : regularHeart}
-          onClick={() => setIsLiked(!isLiked)}
+          onClick={() => handleAddToWish(id, imageUrl, title, price)}
           className="fa-xl absolute top-4 right-4 z-10 cursor-pointer text-green-600"
         />
       </CardHeader>
@@ -76,15 +100,8 @@ export default function ProductCard({ id, imageUrl, title, category, price, rati
           >
             Add To Cart<FontAwesomeIcon className="fa-xl" icon={faCartShopping}></FontAwesomeIcon>
           </Button>
-          <Button
-            className="text-sm px-4"
-            variant="bordered"
-            size="sm"
-            color="default"
-            radius="sm"
-            onPress={() => handleAddToWish(id, imageUrl, title, price)}
-          >
-            Add To WishList
+          <Button className="text-sm px-4" variant="bordered" size="sm" color="default" radius="sm">
+            View Details
           </Button>
         </div>
       </CardFooter>
