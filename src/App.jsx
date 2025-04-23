cbca7196527143227b1281b85672b776dbadaca6
Pravin <pravin.padalkar@techturbine.com>
2025-04-23 10:10:27 +0530

    Added Dynamic Routine for Products

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,19 @@ import Login from "./pages/Login";
 import WishList from "./pages/WishList";
 import Hero from "./pages/Hero";
 import OrderHistory from "./pages/OrderHistory";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/abc" element={<Home />}>
           <Route path="" element={<Hero/>}/>
           <Route path="productlist" element={<ProductList/>}/>
           <Route path="cartlist" element={<CartList />} />
           <Route path="wishlist" element={<WishList />} />
           <Route path="orderhistory" element={<OrderHistory />} />
+          <Route path="product/:id" element={<ProductDetails />} />
         </Route>
         <Route path="/login" element={<Login/>}/>
       </Routes>
