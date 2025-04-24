a50b5da892ac521111b5016d1a142d18d51f40de
Pravin <pravin.padalkar@techturbine.com>
2025-04-24 14:52:14 +0530

    some minor changes

--- src/utilities/MyPagination.jsx
+++ src/utilities/MyPagination.jsx
@@ -1,17 +1,17 @@
 import { Pagination } from "@heroui/react";
 import React from "react";
 
-export default function MyPagination({ total, page, setPage }) {
-    console.log(page)
+export default function MyPagination({ total, setPage }) {
   return (
     <Pagination
       showControls
       showShadow
       color="warning"
       variant="bordered"
-      initialPage={page}
+      initialPage={1}
       total={total}
       onChange={(e)=>setPage(e)}
+      className="mx-auto"
     />
   );
 }
