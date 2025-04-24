1ffca945ada06935b420c35e9749d3ba42650fd4
Pravin <pravin.padalkar@techturbine.com>
2025-04-24 14:52:54 +0530

    Implemented Filter functionality with custom hook

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { useOutletContext } from "react-router";
 import MyPagination from "../utilities/MyPagination";
+import SearchInput from "../utilities/SearchInput";
+import SelectInput from "../utilities/SelectInput";
+import useFilter from '../hooks/useFilter'
 export default function ProductList() {
   const states = useOutletContext();
   const [productList, setProductList] = states.productState;
   const [page, setPage] = useState(1);
   const NoOfItems = 4;
+
+
+  const [filteredList,setSearchQuery] = useFilter(productList,(data)=>data.title)
+  const [filteredList2,setSelectQuery] = useFilter(productList,(data)=>data.category)
+  console.log(filteredList2)
   useEffect(() => {
     if (!productList.length) {
       fetch("https://fakestoreapi.com/products")
@@ -17,15 +25,18 @@ export default function ProductList() {
   return (
     <section>
       <h1 className="text-2xl font-bold  text-custom my-4 bg-custom-400">Products List</h1>
-      <div className="flex justify-end items-center min-h-16 my-4">
+      <div className="flex flex-wrap justify-between gap-8 my-8">
+        <div className="flex flex-wrap sm:flex-nowrap items-center w-[600px] gap-6">
+          <SearchInput setSearchQuery={setSearchQuery} />
+          <SelectInput setSelectQuery={setSelectQuery} />
+        </div>
         <MyPagination
-          total={Math.floor(productList.length / NoOfItems)}
-          page={page}
+          total={Math.floor(filteredList.length / NoOfItems)}
           setPage={setPage}
         />
       </div>
       <div className="grid grid-cols-custom-300 gap-x-4 gap-y-8">
-        {productList
+        {filteredList
           .slice((page - 1) * NoOfItems, page * NoOfItems)
           .map(({ id, image, title, category, price, rating }) => (
             <ProductCard
